d9e79705202ac93731aa8385ba26e71d2ce4c627
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 15:52:30 -0700

    Polish: higher res, tidy legend, add title

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -10,13 +10,14 @@ colnames(sum) <- c("TCA", "MCW", "Frequency", "Neighbors")
 
 stats = merge(stats, sum)
 
-png('MCWvTCA.png', w=2000, h=500)
+png('MCWvTCA.png', w=4000, h=1000, type="cairo", res=150)
 ggplot(stats, aes(x=TCA, y=MCW, size=Frequency)) +
-    geom_point() +
+    geom_point(shape=19) +
     geom_abline(intercept=25*6,slope=-1) +
-    geom_smooth() +
-    geom_label(aes(label=Player), alpha=0.65, stats[stats$Frequency==1 & stats$Neighbors==1,], hjust=0, vjust=0.5, nudge_x=.1, size=3) +
+    geom_smooth(show.legend=F) +
+    geom_label(aes(label=Player), alpha=0.65, stats[stats$Frequency==1 & stats$Neighbors==1,], hjust=0, vjust=0.5, size=3) +
     scale_x_continuous(breaks=seq(0,150,10), minor_breaks = seq(0,150)) +
     scale_y_continuous(breaks=seq(0,35,10), minor_breaks = seq(0,35)) +
-    coord_fixed()
+    coord_fixed() +
+    labs(title="MCW vs TCA in LL72")
 dev.off()
