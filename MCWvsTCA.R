8fdf81cd8fbe6e5a67b4fa11d6beed2fad043849
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 16:19:08 -0700

    Further visual refinement, add small version
    Move legend to bottom, improve limits/expand
    
    Also add a smaller version for thumbnail on the forums

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -12,14 +12,20 @@ colnames(sum) <- c("TCA", "MCW", "Frequency", "Neighbors")
 
 stats = merge(stats, sum)
 
-png('MCWvTCA.png', w=4000, h=1000, type="cairo", res=150)
-ggplot(stats, aes(x=TCA, y=MCW, size=Frequency)) +
+p = ggplot(stats, aes(x=TCA, y=MCW, size=Frequency)) +
     geom_point(shape=19) +
     geom_abline(intercept=25*6,slope=-1) +
     geom_smooth(show.legend=F) +
     geom_label(aes(label=Player), alpha=0.65, stats[stats$Frequency==1 & stats$Neighbors==1,], hjust=0, vjust=0.5, size=3) +
-    scale_x_continuous(breaks=seq(0,150,10), minor_breaks = seq(0,150)) +
-    scale_y_continuous(breaks=seq(0,35,10), minor_breaks = seq(0,35)) +
+    scale_x_continuous(breaks=seq(0,150,10), minor_breaks = seq(0,150), limits=c(-2,152), expand=c(0,0)) +
+    scale_y_continuous(breaks=seq(0,40,10), minor_breaks = seq(0,40), limits=c(-2,42), expand=c(0,0)) +
     coord_fixed() +
-    labs(title="Number of MCWAs vs TCA in LL72")
+    labs(title="Number of MCWAs vs TCA in LL72") +
+    theme(legend.position="bottom")
+
+png('MCWvTCA.png', w=2560, h=900, type="cairo", res=96)
+p
+dev.off()
+png('MCWvTCA_small.png', w=600, h=250, type="cairo",res=48)
+p
 dev.off()
