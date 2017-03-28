4783d82f9d249903ea710a6295753265a7ed936a
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 15:56:45 -0700

    Elaborate on title, and add comment

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -5,6 +5,8 @@ neighbor_limit = 5
 
 stats = read.csv('LL72_Leaguewide_MD25.csv')
 sum = aggregate(Season ~ TCA + MCW, stats, length)
+# This is pretty ugly, but does a decent job of highlighting interesting bits
+# It just finds anyone who doesn't have close neighbors along the TCA axis (that is, people with a similar number of TCA who got the same number of MCWAs)
 sum = adply(sum, 1, function(x) { sum(sum[sum$MCW==x$MCW & sum$TCA > x$TCA-neighbor_limit & sum$TCA < x$TCA+neighbor_limit,]$Season) })
 colnames(sum) <- c("TCA", "MCW", "Frequency", "Neighbors")
 
@@ -19,5 +21,5 @@ ggplot(stats, aes(x=TCA, y=MCW, size=Frequency)) +
     scale_x_continuous(breaks=seq(0,150,10), minor_breaks = seq(0,150)) +
     scale_y_continuous(breaks=seq(0,35,10), minor_breaks = seq(0,35)) +
     coord_fixed() +
-    labs(title="MCW vs TCA in LL72")
+    labs(title="Number of MCWAs vs TCA in LL72")
 dev.off()
