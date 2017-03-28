2c6809676c54e1aee8d0610e2c74230623cb3321
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 16:53:42 -0700

    Output neighbor count, and exclude self
    Subtract one, so neighbors is actually neighbors, instead of
    neighbors and self. Just makes a little nicer.

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -7,8 +7,9 @@ stats = read.csv('LL72_Leaguewide_MD25.csv')
 sum = aggregate(Season ~ TCA + MCW, stats, length)
 # This is pretty ugly, but does a decent job of highlighting interesting bits
 # It just finds anyone who doesn't have close neighbors along the TCA axis (that is, people with a similar number of TCA who got the same number of MCWAs)
-sum = adply(sum, 1, function(x) { sum(sum[sum$MCW==x$MCW & sum$TCA > x$TCA-neighbor_limit & sum$TCA < x$TCA+neighbor_limit,]$Season) })
+sum = adply(sum, 1, function(x) { sum(sum[sum$MCW==x$MCW & sum$TCA > x$TCA-neighbor_limit & sum$TCA < x$TCA+neighbor_limit,]$Season) - 1 })
 colnames(sum) <- c("TCA", "MCW", "Frequency", "Neighbors")
+print(paste("Found",nrow(sum[sum$Neighbors==0,]),"outliers"))
 
 stats = merge(stats, sum)
 
@@ -19,7 +20,7 @@ p = ggplot(stats, aes(x=TCA, y=MCW, size=Frequency,color=MCW/TCA)) +
     geom_smooth(show.legend=F) +
     annotate("text",x=41,y=41,label="MCW > TCA       MCW < TCA",fontface="bold") +
     annotate("text",x=109,y=41,label="Maximum Possible",fontface="bold",angle=-45,hjust=0,vjust=-0.25) +
-    geom_label(aes(label=Player), alpha=0.65, stats[stats$Frequency==1 & stats$Neighbors==1,], hjust=0, vjust=0.5, size=3) +
+    geom_label(aes(label=Player), alpha=0.65, stats[stats$Frequency==1 & stats$Neighbors==0,], hjust=0, vjust=0.5, size=3) +
     scale_x_continuous(breaks=seq(0,150,10), minor_breaks = seq(0,150), limits=c(-2,152), expand=c(0,0)) +
     scale_y_continuous(breaks=seq(0,40,10), minor_breaks = seq(0,40), limits=c(-2,42), expand=c(0,0)) +
     scale_color_gradient2(limits=c(0,2),midpoint=1,mid="black") +
