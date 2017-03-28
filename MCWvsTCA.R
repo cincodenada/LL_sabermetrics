9123ebac67491259f80fa7badd9756f57ace4b50
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 14:48:29 -0700

    Add max line

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -5,5 +5,7 @@ sum = aggregate(Season ~ TCA + MCW, stats, length)
 colnames(sum) <- c("TCA", "MCW", "Frequency")
 
 png('MCWvTCA.png', w=1000, h=500)
-ggplot(sum, aes(x=TCA, y=MCW, size=Frequency)) + geom_point()
+ggplot(sum, aes(x=TCA, y=MCW, size=Frequency)) +
+    geom_point() +
+    geom_abline(intercept=25*6,slope=-1)
 dev.off()
