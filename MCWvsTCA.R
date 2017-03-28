54d50ae7844a8528f4f9ea71cc7a97d413ccd5d6
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 14:53:01 -0700

    Merge sum back into stats

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -4,8 +4,10 @@ stats = read.csv('LL72_Leaguewide_MD25.csv')
 sum = aggregate(Season ~ TCA + MCW, stats, length)
 colnames(sum) <- c("TCA", "MCW", "Frequency")
 
+stats = merge(stats, sum)
+
 png('MCWvTCA.png', w=1000, h=500)
-ggplot(sum, aes(x=TCA, y=MCW, size=Frequency)) +
+ggplot(stats, aes(x=TCA, y=MCW, size=Frequency)) +
     geom_point() +
     geom_abline(intercept=25*6,slope=-1)
 dev.off()
