a5420c64d91159731cbd8806c4714ab645e38e49
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 15:26:16 -0700

    Add average line

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -14,5 +14,6 @@ png('MCWvTCA.png', w=2000, h=1000)
 ggplot(stats, aes(x=TCA, y=MCW, size=Frequency)) +
     geom_point() +
     geom_abline(intercept=25*6,slope=-1) +
+    geom_smooth() +
     geom_text(aes(label=Player), stats[stats$Frequency==1 & stats$Neighbors==1,], nudge_y=0.333, size=4)
 dev.off()
