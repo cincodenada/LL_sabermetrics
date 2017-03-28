6eeba1d459d5c60cb4fdf3493b3c716930528296
Joel Bradshaw <cincodenada@gmail.com>
2017-03-28 16:35:48 -0700

    Colors!
    Also add line for MCW > TCA

--- MCWvsTCA.R
+++ MCWvsTCA.R
@@ -12,13 +12,15 @@ colnames(sum) <- c("TCA", "MCW", "Frequency", "Neighbors")
 
 stats = merge(stats, sum)
 
-p = ggplot(stats, aes(x=TCA, y=MCW, size=Frequency)) +
+p = ggplot(stats, aes(x=TCA, y=MCW, size=Frequency,color=MCW/TCA)) +
     geom_point(shape=19) +
     geom_abline(intercept=25*6,slope=-1) +
+    geom_abline(intercept=0,slope=1) +
     geom_smooth(show.legend=F) +
     geom_label(aes(label=Player), alpha=0.65, stats[stats$Frequency==1 & stats$Neighbors==1,], hjust=0, vjust=0.5, size=3) +
     scale_x_continuous(breaks=seq(0,150,10), minor_breaks = seq(0,150), limits=c(-2,152), expand=c(0,0)) +
     scale_y_continuous(breaks=seq(0,40,10), minor_breaks = seq(0,40), limits=c(-2,42), expand=c(0,0)) +
+    scale_color_gradient2(limits=c(0,2),midpoint=1,mid="black") +
     coord_fixed() +
     labs(title="Number of MCWAs vs TCA in LL72") +
     theme(legend.position="bottom")
